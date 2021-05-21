0dbaea1512bfca7514c5272b6f258288eaf46704
Babakailash <nathkailash2020@gmail.com>
2021-05-21 08:20:40 +0530

    Added:Multiple Address Book

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -13,7 +13,8 @@ public class AddressBookMain {
 
             System.out.println("1.Add Contact");
             System.out.println("2.Edit Contact");
-            System.out.println("3.Exit");
+            System.out.println("3.Delete Contact");
+            System.out.println("4.Exit");
             System.out.println("Enter Choice: ");
 
             int option = sc.nextInt();
@@ -22,21 +23,35 @@ public class AddressBookMain {
             {
                 case 1:
                     addressBook.addContactDetails();
-                    break;
-
-                case 2:
-                    System.out.println("Enter the Person First name to edit details: ");
-                    String person_name = sc.next();
-
-                    boolean b = addressBook.editContactDetails(person_name);
-                    if (b == true) {
-                        System.out.println("Details Updated");
-                    } else {
-                        System.out.println("Contact Not Found");
+                    int noOfContacts = sc.nextInt();
+                    for(int i = 0; i < noOfContacts; i++) {
+                        addressBook.addContactDetails();
                     }
                     break;
 
+
+                case 2:
+                    System.out.println("Enter the Person First name to edit details: ");
+                    String personName = sc.next();
+
+                    boolean listEdited = addressBook.editContactDetails(personName);
+                    if (listEdited) {
+                        System.out.println("List Edited Successfully");
+                    } else {
+                        System.out.println("List Cannot be Edited");
+                    }
+                    break;
                 case 3:
+                    System.out.println("Enter the Contact to be deleted:");
+                    String firstName = sc.next();
+                    boolean listDeleted = addressBook.deleteContact(firstName);
+                    if (listDeleted) {
+                        System.out.println("Deleted Contact from the List");
+                    } else {
+                        System.out.println("List Cannot be Deleted");
+                    }
+                    break;
+                case 4:
                     flag =false;
                     break;
             }
